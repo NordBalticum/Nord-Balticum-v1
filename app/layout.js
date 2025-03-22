b474de8ed9c9404c4e3ce841ae86259751e86b6f
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 09:13:45 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "@/styles/theme.css";
 
 import { Inter } from "next/font/google";
 import { DefaultSeo } from "next-seo";
-import SEO from "@/next-seo.config";
+import SEO from "@/next-seo-config";
 
 import { SupabaseProvider } from "@/context/SupabaseContext";
 import { MagicLinkProvider } from "@/context/MagicLinkContext";
