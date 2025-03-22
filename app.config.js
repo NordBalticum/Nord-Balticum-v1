b8415bbbbbf296473907ce67c570c707dec3fd3e
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 08:24:00 +0200

    Add files via upload

---
+++ app.config.js
@@ -0,0 +1,22 @@
+export default {
+  name: "NordBalticum",
+  short_name: "NB Wallet",
+  description: "Next-generation Web3 financial hub.",
+  theme_color: "#0A1F44",
+  background_color: "#0A1F44",
+  orientation: "portrait",
+  display: "standalone",
+  start_url: "/",
+  icons: [
+    {
+      src: "/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png"
+    },
+    {
+      src: "/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png"
+    }
+  ]
+};
