1781d9a63756a399c12517423d7b15239bdac7d1
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 09:25:48 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,42 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    appDir: false
-  }
+// app/layout.js
+import "@/styles/globals.css";
+import "@/styles/theme.css";
+import { Inter } from "next/font/google";
+import { DefaultSeo } from "next-seo";
+import SEO from "@/next-seo.config";
+
+import { SupabaseProvider } from "@/context/SupabaseContext";
+import { MagicLinkProvider } from "@/context/MagicLinkContext";
+import { AuthProvider } from "@/context/AuthContext";
+import { EthersProvider } from "@/context/EthersContext";
+import { EthersTokenProvider } from "@/context/EthersTokenContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "NordBalticum",
+  description: "The Premium Web3 Wallet & Financial Ecosystem",
 };
 
-module.exports = nextConfig;
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <DefaultSeo {...SEO} />
+      </head>
+      <body className={inter.className} suppressHydrationWarning={true}>
+        <SupabaseProvider>
+          <MagicLinkProvider>
+            <AuthProvider>
+              <EthersProvider>
+                <EthersTokenProvider>
+                  {children}
+                </EthersTokenProvider>
+              </EthersProvider>
+            </AuthProvider>
+          </MagicLinkProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
